6a99f5fe2a334e05e2fa2aca085636d11cb343e6
Jordi Canto Galvez <jordi@prensalink.com>
2020-10-26 21:22:10 +0100

    discord-clone start

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>This is a Sidebar</h2>
+        </div>
+    )
+}
+
+export default Sidebar

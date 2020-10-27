a61b3c9dac6d324b392dc89906561696d72d826e
Jordi Canto Galvez <jordi@prensalink.com>
2020-10-27 10:52:12 +0100

    Sidebar Header and Channel

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="app">
-     <h1>Lets Build a DISCORD CLONE</h1>
-    <Sidebar />
-    {/* Chat */}
+      <Sidebar />   
+      {/* Chat */}
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,31 @@
-import React from "react"
-import "../styles/Sidebar.css"
+import React from "react";
+import "../styles/Sidebar.css";
+import SidebarChannel from "../components/SidebarChannel";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h2>This is a Sidebar</h2>
+            <div className="sidebar__top">
+                <h3>Chicho</h3> 
+                <ExpandMoreIcon />
+            </div>
+            <div className="sidebar__channels">
+                <div className="sidebar__channelHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h3>Channels</h3>
+                    </div>
+                    <AddIcon className="sidebar__addChannel" />
+                </div>
+                <div className="sidebar__channelsList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>
+            </div>
         </div>
     )
 }
